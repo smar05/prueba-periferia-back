becc9889fc4e7b21121caeadbdb7677ca8e35ffa
Ricardo Mantilla <mantillasanchezr@gmail.com>
2024-05-10 22:03:14 -0500

    Archivos iniciales

---
+++ src/main/java/com/example/periferiaback/PeriferiaBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.periferiaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeriferiaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeriferiaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/periferiaback/PeriferiaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.periferiaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeriferiaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
